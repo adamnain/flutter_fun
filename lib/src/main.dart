c4db3619691d5228a948800343b1430e2498d7b9
adamnain <noltambahnol1201git>
2019-11-09 21:45:40 +0700

    chart

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,43 +1,55 @@
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    home: new MyApp(),
-  ));
-}
+class SimpleBarChart extends StatelessWidget {
+  final List<charts.Series> seriesList;
+  final bool animate;
 
-class MyApp extends StatefulWidget {
-  @override
-  _State createState() => new _State();
-}
+  SimpleBarChart(this.seriesList, {this.animate});
 
-class _State extends State<MyApp> {
+  /// Creates a [BarChart] with sample data and no transition.
+  factory SimpleBarChart.withSampleData() {
+    return new SimpleBarChart(
+      _createSampleData(),
+      // Disable animations for image tests.
+      animate: false,
+    );
+  }
 
-  String _value = 'Nothing Yet';
-
-  void _onPressed() => setState(() => _value = new DateTime.now().toString());
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Name here'),
-      ),
-      body: new Container(
-          padding: new EdgeInsets.all(32.0),
-          child: new Center(
-            child: new Column(
-              children: <Widget>[
-                new Text(_value),
-                new IconButton(
-                  icon: new Icon(Icons.timer),
-                  onPressed: _onPressed,
-                  tooltip: 'Click me',
-                )
-              ],
-            ),
-          )
-      ),
+    return new charts.BarChart(
+      seriesList,
+      animate: animate,
     );
   }
+
+  /// Create one series with sample hard coded data.
+  static List<charts.Series<OrdinalSales, String>> _createSampleData() {
+    final data = [
+      new OrdinalSales('2014', 5),
+      new OrdinalSales('2015', 25),
+      new OrdinalSales('2016', 100),
+      new OrdinalSales('2017', 75),
+    ];
+
+    return [
+      new charts.Series<OrdinalSales, String>(
+        id: 'Sales',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (OrdinalSales sales, _) => sales.year,
+        measureFn: (OrdinalSales sales, _) => sales.sales,
+        data: data,
+      )
+    ];
+  }
+}
+
+/// Sample ordinal data type.
+class OrdinalSales {
+  final String year;
+  final int sales;
+
+  OrdinalSales(this.year, this.sales);
 }
