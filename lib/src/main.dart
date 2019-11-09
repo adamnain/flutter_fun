3df82ffcb0895b489994c50ec7debb58a1e70b33
adamnain <noltambahnol1201git>
2019-11-09 19:42:21 +0700

    popumenubutton

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -11,11 +11,37 @@ class MyApp extends StatefulWidget {
   _State createState() => new _State();
 }
 
+enum Animals{Cat, Dog, Bird, Lizard, Fish}
+
 class _State extends State<MyApp> {
 
-  double _value = 0.0;
+  Animals _selected = Animals.Cat;
+  String _value = 'Make a Selection';
+  List<PopupMenuEntry<Animals>> _items = new List<PopupMenuEntry<Animals>>();
 
-  void _onChanged(double value) => setState(() => _value = value);
+
+  @override
+  void initState() {
+    for(Animals animal in Animals.values) {
+      _items.add(new PopupMenuItem(
+        child: new Text(_getDisplay(animal),),
+        value: animal,
+      ));
+    }
+  }
+
+  void _onSelected(Animals animal) {
+    setState((){
+      _selected = animal;
+      _value = 'You Selected ${_getDisplay(animal)}';
+    });
+  }
+
+  String _getDisplay(Animals animal) {
+    int index = animal.toString().indexOf('.');
+    index++;
+    return animal.toString().substring(index);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,21 +52,19 @@ class _State extends State<MyApp> {
       body: new Container(
           padding: new EdgeInsets.all(32.0),
           child: new Center(
-            child: new Column(
+            child: new Row(
               children: <Widget>[
-                new Slider(value: _value, onChanged: _onChanged),
                 new Container(
-                  padding: new EdgeInsets.all(32.0),
-                  child:  new LinearProgressIndicator(
-                    value: _value,
-                    valueColor: new AlwaysStoppedAnimation<Color>(Colors.green),
-                  ),
+                  padding: new EdgeInsets.all(5.0),
+                  child: new Text(_value),
                 ),
-                new Container(
-                  padding: new EdgeInsets.all(32.0),
-                  child:  new CircularProgressIndicator(
-                    value: _value,
-                  ),
+                new PopupMenuButton<Animals>(
+                    child:  new Icon(Icons.input),
+                    initialValue: Animals.Cat,
+                    onSelected: _onSelected,
+                    itemBuilder: (BuildContext context) {
+                      return _items;
+                    }
                 )
               ],
             ),
@@ -48,4 +72,6 @@ class _State extends State<MyApp> {
       ),
     );
   }
+
+
 }
