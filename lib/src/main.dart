1ee0a1dcc214b4481682f3a71144ee9cd1754d95
adamnain <noltambahnol1201git>
2019-11-12 16:50:45 +0700

    stateful widget

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'mywidget.dart';
+import 'clock.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -24,8 +24,8 @@ class _State extends State<MyApp> {
           child: new Center(
             child: new Column(
               children: <Widget>[
-                new Text('Add Widgets Here'),
-                new MyWidget()
+                new Text('Time is:'),
+                new Clock()
               ],
             ),
           )
