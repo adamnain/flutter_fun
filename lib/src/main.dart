0eebc477a399a5b3281dae5ce14c363f40669c92
adamnain <noltambahnol1201git>
2019-11-14 14:03:07 +0700

    navigation pushnamed

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,48 +1,23 @@
 import 'package:flutter/material.dart';
-import 'authenticator.dart';
+import './screens/home.dart';
+import './screens/second.dart';
+import './screens/third.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    home: new MyApp(),
-  ));
-}
+void main() => runApp(new MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _State createState() => new _State();
-}
-
-class _State extends State<MyApp> {
-
-  bool _isAuthenticated;
-
-  void _onAuthenticated(bool value) {
-    setState(() => _isAuthenticated = value);
-  }
-
-
-  @override
-  void initState() {
-    _isAuthenticated = false;
-  }
+class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Name here'),
-      ),
-      body: new Container(
-          padding: new EdgeInsets.all(32.0),
-          child: new Center(
-            child: new Column(
-              children: <Widget>[
-                new Authenticator(onAuthenticated: _onAuthenticated,),
-                new Text('Authenticated: ${_isAuthenticated}')
-              ],
-            ),
-          )
-      ),
+    return new MaterialApp(
+      title: 'Navigation',
+      routes: <String, WidgetBuilder>{
+        //All available pages
+        '/Home': (BuildContext context) => new Home(),
+        '/Second': (BuildContext context) => new Second(),
+        '/Third': (BuildContext context) => new Third(),
+      },
+      home: new Home(), //first page displayed
     );
   }
 }
